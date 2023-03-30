6538400bac1b4ecdee2075b01f07000f269290f2
johalfata <moh.yohalfata@gmail.com>
2023-03-31 00:40:54 +0700

    malam

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -8,9 +8,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -39,9 +40,13 @@ class InvoiceController extends Controller
         //
     }
 
-    public function bulkStore(Request $request) 
+    public function bulkStore(BulkStoreInvoiceRequest $request) 
     {
+        $bulk = collect($request->all())->map(function($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
 
+        Invoice::insert($bulk->toArray());
     }
 
     /**
